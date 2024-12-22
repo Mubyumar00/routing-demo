04da44564a52614afcda95ab89cf52d6f72c312a
Umar Mubarak <mubyumar@gmail.com>
2024-12-22 12:10:10 +0100

    second commit

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,6 +1,16 @@
 import { notFound } from "next/navigation"
 
+function getRandomInt(count: number) {
+    return Math.floor(Math.random() * count);
+}
+
 export default function ReviewDetail( { params } : { params: { productId: string, reviewId: string } } ) {
+    const random = getRandomInt(2)
+    
+    if (random === 1) {
+        throw Error("Error loading review");
+        // throw new Error("Error loading review");
+    };
     if(parseInt(params.reviewId) > 1000) {
         notFound()
     }
