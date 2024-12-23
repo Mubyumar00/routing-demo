eb1ccb483face3d0052c50584916c56237677a75
Umar Mubarak <mubyumar@gmail.com>
2024-12-23 13:28:40 +0100

    third commit

--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/error.tsx
@@ -1,3 +1,4 @@
-export default function ErrorBoundary() {
-    return <div>Error in reviewId</div>
+"use client"
+export default function ErrorBoundary( {error, reset} : {error: Error; reset: () => void} ) {
+    return <div>{error.message} <button onClick={reset}>Try again</button></div>;
 }
--- routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
+++ routing-demo/src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -1,20 +1,36 @@
-import { notFound } from "next/navigation"
+"use client";
+import { notFound } from "next/navigation";
+type PageProps = {
+    params: {
+        productId: string;
+        reviewId: string;
+    };
+};
 
 function getRandomInt(count: number) {
     return Math.floor(Math.random() * count);
 }
 
-export default function ReviewDetail( { params } : { params: { productId: string, reviewId: string } } ) {
-    const random = getRandomInt(2)
-    
-    if (random === 1) {
-        throw Error("Error loading review");
-        // throw new Error("Error loading review");
-    };
-    if(parseInt(params.reviewId) > 1000) {
-        notFound()
+export default function ReviewDetail({ params }: PageProps) {
+    try {
+        const random = getRandomInt(2);
+
+        if (random === 1) {
+            // throw new Error("Error loading review");
+            console.log()
+        }
+
+        if (parseInt(params?.reviewId) > 1000) {
+            notFound();
+        }
+
+        return (
+            <h1>
+                Review {params?.reviewId} for product {params?.productId}
+            </h1>
+        );
+    } catch (error) {
+        console.error(error);
+        return <p>There was an error loading the review. Please try again later.</p>;
     }
-    return <h1>
-        Review {params.reviewId} for product {params.productId}
-    </h1>
 }
